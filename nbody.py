67c573a959f04ba27d3cc8fec95bc9775ae9fd73
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-02-15 17:34:24 +0000

    Add standard Verlet method with initial velocity setup

--- nbody.py
+++ nbody.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.constants import gravitational_constant as G
 import matplotlib.pyplot as plt
+from time import perf_counter
 
 
 EPSILON = 0.001
@@ -9,6 +10,7 @@ EPSILON = 0.001
 class Particle:
     def __init__(self, x, y, z, vx, vy, vz, mass, name=None):
         self.pos = np.array([x, y, z], dtype=np.float64)
+        self.pos_prev = np.array([x, y, z], dtype=np.float64)
         self.vel = np.array([vx, vy, vz], dtype=np.float64)
         self.acc = np.array([0, 0, 0], dtype=np.float64)
         self.acc_prev = np.array([0, 0, 0], dtype=np.float64)
@@ -26,7 +28,7 @@ def create_solar_system():
     r = np.array(([0,0],[0.4,0],[0,0.7],[1,0],[0,1.5],[5.2,0],[0,9.5],[19.2,0],[0,30.1]))
     v = np.array(([0,0],[0,-np.sqrt(1/0.4)],[np.sqrt(1/0.7),0],[0,-1],[np.sqrt(1/1.5),0],[0,-np.sqrt(1/5.2)],[np.sqrt(1/9.5),0],[0,-np.sqrt(1/19.2)],[np.sqrt(1/30.1),0]))
 
-    return [Particle(r[i, 0], r[i,1], 0, v[i,0], v[i,1], 0, m[i,0], name=names[i]) for i in range(len(names))]
+    return [Particle(r[i, 0], r[i,1], 0, v[i,0], v[i,1], 0, m[i,0], name=names[i]) for i in range(2)]
 
 
 def main():
@@ -36,8 +38,23 @@ def main():
     dt = 0.01
     # ax = plt.axes(projection='3d')
     ax = plt.axes()
+    start = perf_counter()
+
     t = 0.0
-    i = 0
+
+## STANDARD VERLET METHOD WITH INITIAL SETUP
+    for p1 in particles:
+        p1.acc[:] = 0.0
+        for p2 in particles:
+            r = p2.pos - p1.pos
+            p1.acc += p2.mass * r \
+                    / (np.linalg.norm(r)**2 + EPSILON**2)**(1.5)
+    for particle in particles:
+        particle.tracker += [particle.pos.copy()]
+        particle.pos_prev[:] = particle.pos[:]
+        particle.pos += particle.vel * dt + 0.5 * particle.acc * dt**2
+    t += dt
+
     while t < 40:
         for p1 in particles:
             p1.acc[:] = 0.0
@@ -47,10 +64,29 @@ def main():
                         / (np.linalg.norm(r)**2 + EPSILON**2)**(1.5)
         for particle in particles:
             particle.tracker += [particle.pos.copy()]
-            particle.pos += particle.vel * dt + 0.5 * particle.acc * dt**2
-            particle.vel += particle.acc * dt
+            temp_pos = particle.pos.copy()
+            particle.pos = 2.0 * particle.pos - particle.pos_prev + particle.acc * dt**2
+            particle.pos_prev[:] = temp_pos[:]
         t += dt
-        i += 1
+
+## VELOCITY VERLET METHOD:
+    # while t < 40:
+        # for p1 in particles:
+            # p1.acc[:] = 0.0
+            # for p2 in particles:
+                # r = p2.pos - p1.pos
+                # p1.acc += p2.mass * r \
+                        # / (np.linalg.norm(r)**2 + EPSILON**2)**(1.5)
+        # for particle in particles:
+            # particle.tracker += [particle.pos.copy()]
+            # particle.pos += particle.vel * dt + 0.5 * particle.acc * dt**2
+            # particle.vel += particle.acc * dt
+        # t += dt
+
+    end = perf_counter()
+
+    print(f"Time to complete: {end-start}")
+
 
     for p in particles:
         xdata = np.array(p.tracker)[:,0]
