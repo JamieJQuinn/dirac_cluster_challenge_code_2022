2b8d031c7b8620387f65f28eb1a71b4a4bc8f23b
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-05-25 15:50:08 +0100

    Add temperature to stams to make it a nice convection simulation

--- stams.py
+++ stams.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from numpy.random import default_rng
 from time import perf_counter
+
+
+SEED = 0
 
 
 def handle_corners(x):
@@ -12,6 +16,9 @@ def handle_corners(x):
 
 
 def set_dirichlet_bcs(x, bcs):
+    """
+    Apply Dirichlet boundary conditions; conditions that specify a particular value of x on the boundary.
+    """
     left, top, right, bottom = bcs
     x[ 0, :] = -x[1,:] + 2.*left
     x[-1, :] = -x[-2,:] + 2.*right
@@ -21,6 +28,9 @@ def set_dirichlet_bcs(x, bcs):
 
 
 def set_von_neumann_bcs(x):
+    """
+    Apply von Neumann boundary conditions; conditions that specify a particular value of the *derivative* of x on the boundary.
+    """
     x[ 0, :] = x[ 1, :]
     x[-1, :] = x[-2, :]
     x[:,  0] = x[:,  1]
@@ -28,24 +38,64 @@ def set_von_neumann_bcs(x):
     handle_corners(x)
 
 
-def set_periodic_bcs(x):
+def set_periodic_bcs_x(x):
+    """
+    Apply periodic boundary conditions in x; speedy things go in, speedy things go out
+    """
     x[ 0, :] = x[-2, :]
     x[-1, :] = x[1, :]
+
+
+def set_periodic_bcs_y(x):
+    """
+    Apply periodic boundary conditions in y
+    """
     x[:,  0] = x[:, -2]
     x[:, -1] = x[:, 1]
+
+
+def set_periodic_bcs(x):
+    """
+    Apply periodic boundary conditions in x and y
+    """
+    set_periocic_bcs_x(x)
+    set_periodic_bcs_y(x)
     handle_corners(x)
 
 
 def set_u_bcs(u):
-    set_dirichlet_bcs(u, [0., 0., 0., 0.])
+    bottom = 0.
+    top = 0.
+    u[ :,  0] = -u[:,1] + 2.*bottom
+    u[ :, -1] = -u[:,-2] + 2.*top
+
+    set_periodic_bcs_x(u)
+    handle_corners(u)
 
 
 def set_v_bcs(v):
-    set_dirichlet_bcs(v, [0., 0., 0., 0.])
+    bottom = 0.
+    top = 0.
+    v[ :,  0] = -v[:,1] + 2.*bottom
+    v[ :, -1] = -v[:,-2] + 2.*top
+
+    set_periodic_bcs_x(v)
+    handle_corners(v)
 
 
 def set_pressure_bcs(p):
     set_von_neumann_bcs(p)
+
+
+def set_tmp_bcs(tmp):
+    # Top and bottom are at a fixed temp
+    bottom = 0.
+    top = -1.
+    tmp[ :,  0] = -tmp[:,1] + 2.*bottom
+    tmp[ :, -1] = -tmp[:,-2] + 2.*top
+    # Left and right have zero heat flux
+    tmp[ 0, :] = tmp[ 1, :]
+    tmp[-1, :] = tmp[-2, :]
 
 
 def diffuse(x, x0, dt, diff, nx, ny, dx, dy, set_boundary_fn, n_iterations=100):
@@ -63,25 +113,27 @@ def diffuse(x, x0, dt, diff, nx, ny, dx, dy, set_boundary_fn, n_iterations=100):
 
 def project(u, v, p, div, nx, ny, dx, dy, max_iterations = 100):
     """
-    Calculate pressure required to keep fluid incompressible and add pressure force to flow
+    Calculate pressure required to keep fluid incompressible and add pressure force to velocity
     """
-    # JACOBI ITERATION
+    # Calculate divergence of flow; this is what we want to make zero
     div[1:-1,1:-1] = 0.5*(u[2:,1:-1] - u[:-2,1:-1])/dx + 0.5*(v[1:-1,2:] - v[1:-1,:-2])/dy
-
-    p[:] = 0.
-
     set_dirichlet_bcs(div, [0., 0., 0., 0.])
+
+    # Initialise pressure
+    p[:] = 0.
     set_pressure_bcs(p)
 
     temp = np.zeros_like(p)
 
     D = -2.*(1./dx**2 + 1./dy**2)
 
+    # Perform Jacobi iteration to calculate pressure
     for k in range(max_iterations):
         temp[1:-1,1:-1] = (div[1:-1,1:-1] - (p[:-2,1:-1] + p[2:,1:-1])/dx**2 - (p[1:-1,:-2] + p[1:-1,2:])/dy**2)/D
         p[:] = temp[:]
         set_pressure_bcs(p)
 
+    # Add pressure force to velocity
     u[1:-1,1:-1] -= 0.5*(p[2:,1:-1] - p[:-2,1:-1])/dx
     v[1:-1,1:-1] -= 0.5*(p[1:-1,2:] - p[1:-1,:-2])/dy
 
@@ -90,15 +142,18 @@ def project(u, v, p, div, nx, ny, dx, dy, max_iterations = 100):
 
 
 def advect(q, q0, u, v, dt, dx, dy, nx, ny, set_boundary_fn):
+    """
+    Advect the quantity q (original value in q0) using velocities u and v
+    """
     for i in range(1, nx+1):
         for j in range(1, ny+1):
             x = i-dt/dx*u[i,j]
             y = j-dt/dy*v[i,j]
 
             x = max(x, 0.5)
-            x = min(x, nx+2-0.5)
+            x = min(x, nx+0.5)
             y = max(y, 0.5)
-            y = min(y, ny+2-0.5)
+            y = min(y, ny+0.5)
 
             i0 = int(x)
             j0 = int(y)
@@ -122,21 +177,24 @@ def main():
 
     lx = 1.
     ly = 1.
-    visc = 0.01
+    Pr = 10.
+    Ra = 800.
+    tmp_diff = 1.0
 
     dx = lx/nx
     dy = ly/ny
 
-    dt = 0.1
+    epsilon = 0.01
+
+    dt = 0.001
     # dt = 0.5*min(dx**2/visc, dx)
-    dump_dt = 1
-    total_time = 10.
+    dump_dt = 0.01
+    total_time = 0.5
 
     FP_TYPE = np.float32
 
-    print(f'Numerical viscosity: {dx**2/dt}')
-    print(f'Real viscosity: {visc}')
-    print(f'Explicit dt (diffusion): {dx**2/visc}')
+    print(f'Numerical diffusion: {dx**2/dt}')
+    print(f'Explicit dt (diffusion): {dx**2/Pr}')
     print(f'Explicit dt (CFL): {dx}')
     print(f'dt: {dt}')
 
@@ -144,15 +202,16 @@ def main():
     v = np.zeros_like(u)
     u_prev = np.zeros_like(u)
     v_prev = np.zeros_like(u)
+    tmp = np.zeros_like(u)
+    tmp_prev = np.zeros_like(u)
     pressure = np.zeros_like(u)
+
+    rng = default_rng(SEED)
+    tmp += epsilon * (2 * rng.random(tmp.shape) - 1.0)
 
     x = np.linspace(-dx, lx+dx, nx+2)
     y = np.linspace(-dy, ly+dy, ny+2)
     X, Y = np.meshgrid(x, y, indexing='ij')
-
-    # dens_prev[(X-0.5)**2 + (Y-0.5)**2 < 0.05] = 0.1
-    # u[:] = 0.05*(Y-ly/2)
-    # v[:] = -0.05*(X-lx/2)
 
     set_u_bcs(u)
     set_u_bcs(u_prev)
@@ -173,44 +232,51 @@ def main():
     t = 0.
     step_counter = 0
     time_to_next_dump = 0.
+    dump_counter = 0
     while t < total_time:
-        # if (t > time_to_next_dump):
+        if (t > time_to_next_dump):
             # plot = plt.quiver(x[1:-1], y[1:-1], u[1:-1,1:-1].T, v[1:-1,1:-1].T, animated=True)
-            # if len(plots) == 0:
-                # plt.quiver(x[1:-1], y[1:-1], u[1:-1,1:-1].T, v[1:-1,1:-1].T, animated=True)
-            # plots.append([plot])
-            # print(f"t={t} of {total_time}")
-            # time_to_next_dump += dump_dt
-        u_prev[(X-lx/2.)**2 + (Y-ly/2.)**2 < 0.1**2] = 1.
-        diffuse(u, u_prev, dt, visc, nx, ny, dx, dy, set_u_bcs)
-        diffuse(v, v_prev, dt, visc, nx, ny, dx, dy, set_v_bcs)
+            # vorticity = (v[2:,1:-1] - v[:-2,1:-1])/(2.*dx) - (u[1:-1,2:] - u[1:-1,:-2])/(2.*dy)
+            # plot = plt.pcolormesh(X[1:-1,1:-1], Y[1:-1,1:-1], tmp[1:-1,1:-1], vmin=-1, vmax=1)
+            plot = plt.pcolormesh(X[1:-1,1:-1], Y[1:-1,1:-1], tmp[1:-1,1:-1])
+            # vlimit = max(abs(np.max(vorticity)), abs(np.min(vorticity)))
+            # plot = plt.pcolormesh(X[1:-1,1:-1], Y[1:-1,1:-1], vorticity, cmap='RdBu', vmax=vlimit, vmin=-vlimit)
+            plt.colorbar(plot)
+            plt.quiver(x, y, u.T, v.T)
+            ax.set_aspect('equal')
+            # plt.xlim(0, lx)
+            # plt.ylim(0, ly)
+
+            fname = f"{dump_counter:04d}.png"
+            plt.savefig(fname)
+            plt.close()
+
+            print(f"t={t} of {total_time}")
+            dump_counter += 1
+            time_to_next_dump += dump_dt
+        # u_prev[(X-lx/2.)**2 + (Y-ly/2.)**2 < 0.1**2] = 1.
+        v_prev[1:-1,1:-1] += Ra*Pr*tmp[1:-1,1:-1]
+        diffuse(u, u_prev, dt, Pr, nx, ny, dx, dy, set_u_bcs)
+        diffuse(v, v_prev, dt, Pr, nx, ny, dx, dy, set_v_bcs)
+        diffuse(tmp, tmp_prev, dt, tmp_diff, nx, ny, dx, dy, set_tmp_bcs)
         project(u, v, u_prev, v_prev, nx, ny, dx, dy)
         u_prev[:] = u[:]
         v_prev[:] = v[:]
+        tmp_prev[:] = tmp[:]
         advect(u, u_prev, u_prev, v_prev, dt, dx, dy, nx, ny, set_u_bcs)
         advect(v, v_prev, u_prev, v_prev, dt, dx, dy, nx, ny, set_v_bcs)
+        advect(tmp, tmp_prev, u_prev, v_prev, dt, dx, dy, nx, ny, set_tmp_bcs)
         project(u, v, u_prev, v_prev, nx, ny, dx, dy)
-        pressure[:] = u_prev[:]
+        # pressure[:] = u_prev[:]
         u_prev[:] = u[:]
         v_prev[:] = v[:]
+        tmp_prev[:] = tmp[:]
         t += dt
         step_counter += 1
 
     end = perf_counter()
     print(f"Time to complete: {end-start}")
-    print(f"FPS: {(total_time/dt)/(end-start)}")
-
-    vorticity = (v[2:,1:-1] - v[:-2,1:-1])/(2.*dx) - (u[1:-1,2:] - u[1:-1,:-2])/(2.*dy)
-    plt.pcolormesh(X[1:-1,1:-1], Y[1:-1,1:-1], pressure[1:-1,1:-1])
-    # vlimit = max(abs(np.max(vorticity)), abs(np.min(vorticity)))
-    # plot = plt.pcolormesh(X[1:-1,1:-1], Y[1:-1,1:-1], vorticity, cmap='RdBu', vmax=vlimit, vmin=-vlimit)
-    # plt.colorbar(plot)
-    plt.quiver(x, y, u.T, v.T)
-    ax.set_aspect('equal')
-    plt.xlim(0, lx)
-    plt.ylim(0, ly)
-    # ani = animation.ArtistAnimation(fig, plots, interval=5, repeat_delay=1000)
-    plt.show()
+    print(f"FPS: {(step_counter)/(end-start)}")
 
 
 if __name__ == "__main__":
